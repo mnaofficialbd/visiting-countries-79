4d535dd6fed84293c0758946d920236564018514
mnaofficial <mna.official92@gmail.com>
2022-05-30 21:11:39 +0600

    countries data load

--- Components/Countries.js
+++ Components/Countries.js
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 export default function Countries() {
-  return (
-    <View>
-      <Text>Countries</Text>
-    </View>
-  )
+    const [countries, setCountries] = useState([]);
+    useEffect(() => {
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res=>res.json())
+        .then(data=>setCountries(data))
+    }, []);
+
+    return (
+        <View>
+            <Text>Countries: {countries.length}</Text>
+        </View>
+    )
 }
