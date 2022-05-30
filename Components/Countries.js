ca8e727893357691a94c08553870c0636201a035
mnaofficial <mna.official92@gmail.com>
2022-05-30 23:47:58 +0600

    input search input setup

--- Components/Countries.js
+++ Components/Countries.js
@@ -1,21 +1,34 @@
-import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import { View, Text, ScrollView, StyleSheet, TextInput} from 'react-native'
 import React, { useState, useEffect } from 'react'
 import Country from './Country';
 
 export default function Countries() {
     const [countries, setCountries] = useState([]);
+    const [searched, setSearched] = useState([]);
+
     useEffect(() => {
         fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
-            .then(data => setCountries(data))
+            .then(data => {
+                setCountries(data);
+                setCountries(data)
+            })
     }, []);
 
+    const handleSearch=text=>{
+        const filtered=countries.filter(country=>country.name.common.includes(text))
+        setSearched(filtered)
+    }
     return (
         <View>
-            <Text style={styles.header}>Countries: {countries.length}</Text>
+            <Text style={styles.header}>Countries: {searched.length}</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={handleSearch}
+            />
             <ScrollView>
                 {
-                    countries.map(country => <Country
+                    searched.map(country => <Country
                         //   key={}
                         country={country}
                     ></Country>)
@@ -31,4 +44,10 @@ const styles = StyleSheet.create({
         fontSize: 40,
         color: 'red'
     },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+      }
 });
